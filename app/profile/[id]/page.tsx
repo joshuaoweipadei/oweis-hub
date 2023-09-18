3715f70d2f52ff576cad5f419dabc30c12d4d258
joshuaoweipadei <oweipadeijoshie@gmail.com>
2023-09-18 12:22:58 -0400

    testing change

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -12,7 +12,7 @@ const page = async ({ params }: Props) => {
   const result = await getUserProjects(params.id, 100) as { user: UserProfile }
 
   if(!result?.user) return (
-    <p className="no-result-text">Failed to fetch user info</p>
+    <p className="no-result-text">Failed to fetch user information</p>
   )
 
   return <ProfilePage user={result?.user}  />
